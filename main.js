5ac673f2b3b102d653a4e7bad52b0f10dc073aac
Coleman <crollins199@gmail.com>
2017-05-01 15:48:36 -0500

    [cr] some decent animation using anime.js

--- main.js
+++ main.js
@@ -17,13 +17,42 @@ $(document).ready(function() {
     }
   });
 
-  var button = document.querySelector('button');
-  button.addEventListener('click', function() {
-    var myMultiTween2 = KUTE.allFromTo(
-          '.letter',
-          {translate:[0,0], rotate: 0},
-          {translate:[0,150], rotate: 360},
-          {transformOrigin: '10% 10%', offset: 200 }
-      ).start();
-  })
+  var animate = document.querySelector('#animate');
+  animate.addEventListener('click', function() {
+    $('.letter').each(function() {
+      $(this).addClass('animate')
+    });
+    var els = document.querySelectorAll('.letter');
+
+    var nodeList = anime({
+      targets: els,
+      translateY: [
+        { value: -100, elasticity: 100 }
+      ],
+      rotate: {
+        value: 360,
+        duration: 100,
+        easing: 'easeInOutSine',
+        elasticity: 100
+      },
+      scale: [
+        { value: 1, duration: 100, elasticity: 100 , easing: 'easeOutExpo'},
+        { value: 75, duration: 100, delay: 50, easing: 'easeOutExpo' },
+        { value: 1, duration: 100, elasticity: 100, easing: 'easeOutExpo' }
+      ],
+      // scaleY: [
+      //   { value: [1.75, 1], duration: 500 },
+      //   { value: 2, duration: 50, delay: 100, easing: 'easeOutExpo' },
+      //   { value: 1, duration: 450 },
+      //   { value: 1.75, duration: 50, delay: 100, easing: 'easeOutExpo' },
+      //   { value: 1, duration: 450 }
+      // ],
+      delay: function(el, i, l) { return i * 90; },
+      duration: 100
+    });
+  });
+  var reload = document.querySelector('#reload');
+  reload.addEventListener('click', function() {
+    location.reload();
+  });
 });
